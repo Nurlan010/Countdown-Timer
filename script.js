d87b6807e613f5a5f486bc4581e68caf464b0118
Nurlan010 <nurlanbabazade1@mail.ru>
2020-09-07 17:46:15 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const daysel=document.querySelector('#days');
+const hoursel=document.querySelector('#hours');
+const minsel=document.querySelector('#mins');
+const secondsel=document.querySelector('#seconds');
+
+const newYear="1 Jan 2021";
+countdown();
+function countdown(){
+const newYearsDate=new Date(newYear);
+const currentDate=new Date();
+
+
+const totalseconds=(newYearsDate - currentDate)/1000;
+
+const days=Math.floor(totalseconds / 3600 / 24);
+const hours=Math.floor(totalseconds/3600) % 24;
+const mins=Math.floor(totalseconds/60) % 60;
+const seconds=Math.floor(totalseconds) % 60;
+
+daysel.innerHTML=days;
+hoursel.innerHTML=formatTime(hours);
+minsel.innerHTML=formatTime(mins);
+secondsel.innerHTML=formatTime(seconds);
+}
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(countdown,1000);
